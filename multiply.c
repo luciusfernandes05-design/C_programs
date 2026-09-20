48be9baa7c01949fc62ea6298c7b73e6b2a5b8fe
Lucius <luciusfernandes05@gmail.com>
2026-09-20 15:11:53 +0530

    added: multiply code

---
+++ multiply.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+int main()
+{
+    int num1;
+    int num2;
+    printf("what is your number?");
+    scanf("%d", &num1);
+    printf("what is your second number?");
+    scanf("%d", &num2);
+    int num3;
+    num3 = num1*num2;
+   printf("result of mulitplication is: %d", num3);
+return 0;
+}
+
